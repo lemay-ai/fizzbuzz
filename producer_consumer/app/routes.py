ec406a9ce7a1be6e97f567e8c82fa62352c759e0
simGudim <simongudim@yahoo.ca>
2020-11-13 11:06:53 -0500

    added ajax

--- producer_consumer/app/routes.py
+++ producer_consumer/app/routes.py
@@ -4,11 +4,8 @@ from app.models import Msg
 from app.utils import *
 import datetime
 import json
-from flask import render_template, request, redirect, url_for, make_response, jsonify
+from flask import render_template, request, jsonify, url_for, make_response
 from sqlalchemy import func, desc
-import time
-
-
 
 
 
@@ -20,29 +17,28 @@ def index():
     avg_time = db.session.query(Msg).filter(Msg.timestamp >= datetime.datetime.now() - datetime.timedelta(seconds=10))\
         .with_entities(func.avg(Msg.time))\
         .scalar()
-    print(avg_time)
+
     return render_template('index.html', title='Home', user=user, max_msg = max_msg, avg_time = round(avg_time, 2))
 
+@app.route('/calculate_result', methods = ["GET"])
+def calculate_result():
+    max_msg = db.session.query(func.max(Msg.msg_num)).scalar()
+    avg_time = db.session.query(Msg).filter(Msg.timestamp >= datetime.datetime.now() - datetime.timedelta(seconds=10))\
+        .with_entities(func.avg(Msg.time))\
+        .scalar()
+    res = jsonify({"msg_num" : "The max message #: {}".format(max_msg), "avg_time" : "The averga time of the calls for the last 10 seconds is {} milliseconds".format(round(avg_time, 2))})
+    return res
 
-## The consumer routess
+
+## The consumer routes
 @app.route('/consumer', methods=["POST"])
 def consumer():
     arrival_time = datetime.datetime.now()
     if request.method == "POST":
         job = app.task_queue.enqueue(db_insert, request.data, arrival_time)
-        res = {"message" : f"The request was completed, job number {job.id}"}
+        res = {"message" : f"The request was completed, job id: {job.id}"}
         return jsonify(res)
     else:
         return "The request method has to be POST"
 
-# ##The producer routes
-# @app.route('/producer', methods = ["GET", "POST"])
-# def producer():
-#     url = "http://localhost:5000/consumer"
-#     headers = {'content-type': 'application/json'}
-#     for i in range(0, 100):
-#         start = datetime.datetime.now()
-#         data = { "msg_num": i, "start" : start}
-#         reqs.post(url, data=json.dumps(data, default = myconverter), headers=headers)
-#     return redirect(url_for("index"))
 
