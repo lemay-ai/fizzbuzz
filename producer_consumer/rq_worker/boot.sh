ea3589cdf8f92a0cafe2d1452a4e6c41e8f8d939
simGudim <simongudim@yahoo.ca>
2020-11-14 00:01:02 -0500

    fixed docker-compose

--- producer_consumer/rq_worker/boot.sh
+++
@@ -1,8 +0,0 @@
-while true; do
-    rq worker --url redis://redis:6379/0
-    if [[ "$?" == "0" ]]; then
-        break
-    fi
-    echo Upgrade command failed, retrying in 5 secs...
-    sleep 5
-done
