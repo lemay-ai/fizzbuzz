5dee145b673e85f768cbd999f982dd7da2a32666
Vaibhav P <43818020+VAIBHAVPATEL97@users.noreply.github.com>
2022-01-26 15:02:27 -0500

    New major changes required.

--- gpt2_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-#print("hello")
-text_generate = pipeline("text-generation")
-input_txt = str(input(" Enter your text to develop some context:"))
-generated_txt = text_generate(input_txt, max_length = 500, do_sample=False)[0]
-print(generated_txt['generated_text'])
-print(type(generated_txt['generated_text']))
-
-
